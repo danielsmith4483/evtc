2bbc119f202eee2c1ede677868a228c0a2c700a5
Daniel Smith <danielsmith4483@gmail.com>
2017-12-22 18:23:21 +0000

    Add unit testing capabilities and tweak linting settings

---
+++ test/encounter.js
@@ -0,0 +1,21 @@
+import { assert } from 'chai'
+import Encounter from 'encounter';
+import Squad from 'squad';
+
+describe('Encounter', () => {
+  describe('constructor', () => {
+    const buildVersion = "buildver";
+    const instanceId = 1;
+
+    const encounter = new Encounter(buildVersion, instanceId);
+    it('should initialize build version', () => {
+      assert.equal(encounter.buildVersion, buildVersion);
+    });
+    it('should initilalize instance ID', () => {
+      assert.equal(encounter.bossInstanceId, instanceId);
+    });
+    it('should initialize the squad object', () => {
+      assert.instanceOf(encounter.squad, Squad);
+    });
+  });
+});
