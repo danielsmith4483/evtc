350b82e2551e9d4dc8f8f794fd3dba9121b60493
Daniel Smith <danielsmith4483@gmail.com>
2017-12-27 22:23:10 +0000

    Turn smarter-buffer into wrapper class

--- program.js
+++ program.js
@@ -1,5 +1,7 @@
-require('babel-polyfill');
 var evtc = require('./lib/index.js');
 
-//const encounter = evtc.fromFile('../20171002-210432.evtc');
-const encounter = evtc.fromUrl("https://storage.googleapis.com/logwars2-default/20171211-230356.evtc.zip");
+evtc.fromFile('../20171002-210432.evtc').catch(err => {
+  console.log(err);
+});
+//const encounter = evtc.fromUrl("https://storage.googleapis.com/logwars2-default/20171211-230356.evtc.zip");
+
--- src/smarter-buffer.js
+++ src/smarter-buffer.js
@@ -1,38 +1,25 @@
 const SmartBuffer = require('smart-buffer').SmartBuffer;
 
-module.exports = class SmarterBuffer extends SmartBuffer {
-  constructor(options) {
-    super(options)
+module.exports = class SmarterBuffer {
+  constructor(buffer) {
+    this.smartBuffer = SmartBuffer.fromBuffer(buffer);
+  }
 
-    const _stack = []
-
-    this.push_offset = function() {
-      _stack.push(this.readOffset);
-    };
-
-    this.pop_offset = function() {
-      this.readOffset = _stack.pop();
-    };
+  static fromBuffer(buffer) {
+    return new this(buffer);
   }
 
   skip(numBytes) {
-    this.readOffset += numBytes;
+    this.smartBuffer.readOffset += numBytes;
   }
 
   readUIntLE(numBytes) {
-    const num = this.internalBuffer.readUIntLE(this.readOffset, numBytes);
+    const num = this.smartBuffer.internalBuffer.readUIntLE(this.readOffset, numBytes);
     this.skip(numBytes);
     return num;
   }
 
   readString(numBytes) {
-    return super.readString(numBytes).replace(/\0+$/, '');;
-  }
-
-  static fromBuffer(buff, encoding) {
-    return new SmarterBuffer({
-      buff: buff,
-      encoding: encoding
-    });
+    return this.smartBuffer.readString(numBytes).replace(/\0+$/, '');;
   }
 }
