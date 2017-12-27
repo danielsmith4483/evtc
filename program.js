3314bb5172c4f792e3e6c99f20a069c073fc93db
Daniel Smith <danielsmith4483@gmail.com>
2017-12-27 20:33:56 +0000

    Add polyfill transform for babel-runtime

--- program.js
+++ program.js
@@ -1,5 +1,4 @@
-require('babel-polyfill');
-var evtc = require('./lib/index.js');
-console.log(evtc);
+//require('babel-polyfill');
+//var evtc = require('./lib/index.js');
 
-const encounter = evtc.fromFile('../20171002-210432.evtc');
+//const encounter = evtc.fromFile('../20171002-210432.evtc');
